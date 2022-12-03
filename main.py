9025c67a32b48f490c1312085154b8de3d952808
MarianneIB <38662581+MarianneIB@users.noreply.github.com>
2022-12-03 15:44:26 +0100

    Added Number Feature function

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print("This is a new beginning.")
 #input:
 #output: array with numbers between 32 and 255
+def numberfeature():
+    m = 32
+    n = 255
+    num = []
+    for i in range (m,n+1):
+        num.append(i)
+    return num
+
+print(numberfeature())
